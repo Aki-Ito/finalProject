f7ce60141496267fff82412ec43f52600ae4e29f
Akitaka Ito <a2ki0ta0ka0@icloud.com>
2020-10-03 20:16:51 +0900

    Initial Commit

---
+++ finalProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  finalProject
+//
+//  Created by 伊藤明孝 on 2020/10/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
